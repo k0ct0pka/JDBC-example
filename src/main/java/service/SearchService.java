ff174dcc18088fe630cef03e8c65cede48544956
Lenovo <dima.kozak2008@gmail.com>
2024-11-01 18:48:18 -0700

    patch added Search Feature 2.2

---
+++ src/main/java/service/SearchService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dto.Car;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.*;
+
+public class SearchService {
+    private CarService carService;
+    public List<Car> searchCars(HttpServletRequest req) {
+
+        return null;
+    }
+}
